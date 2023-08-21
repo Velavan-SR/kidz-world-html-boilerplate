74a2203e16700021c513e44a8c2f79b6c4ef07a4
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2023-08-21 13:48:10 +0530

    Lab on making the site functional part 1 completed

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 let buttons = document.querySelectorAll('.button');
 let cartValue = document.getElementById('cart-value')
 let itemsArray = [];
+let totalPrice = 0;
 
 buttons.forEach((button,index) => {
-    button.addEventListener('click',()=>{
+    button.addEventListener('click',(event)=>{
         let currentValue = parseInt(cartValue.textContent);
         cartValue.textContent = currentValue + 1;
+        const h3element = event.currentTarget.parentNode.previousElementSibling.querySelector('h3');
+        const pelement = event.currentTarget.parentNode.querySelector('p');
+
+        const itemName = h3element.textContent;
+        const existingObject = itemsArray.find(obj => obj.itemName === itemName);
+        
+        if (existingObject){
+            existingObject.quantity++
+        }
+
+        else{
+            const newObj = {
+                itemName: itemName,
+                quantity: 1,
+            }
+            itemsArray.push(newObj)
+        }
+
+        totalPrice = totalPrice+parseFloat(pelement.textContent.slice(1));
     })
 });
+
+document.getElementById('cart').addEventListener('click',()=>{
+    itemsArray.forEach((obj) => {
+        console.log(obj)
+    })
+    console.log("The total payable amount is $"+totalPrice)
+})
